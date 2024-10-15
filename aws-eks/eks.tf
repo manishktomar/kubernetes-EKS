3b90c99bb68350e54e31f4747e613d6fabdca7f4
Manish Tomar <108394714+manishktomar@users.noreply.github.com>
2024-10-15 22:48:08 +0530

    aws-eks-cluster

---
+++ aws-eks/eks.tf
@@ -0,0 +1,16 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.eks_subnet_a.id,
+      aws_subnet.eks_subnet_b.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy
+  ]
+}
